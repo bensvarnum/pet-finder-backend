3a05b4336ce7dbc49b19b589177eea590bdd4754
Daniel Gallagher <danielbgallagher@gmail.com>
2021-01-10 16:12:45 -0500

    color route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,19 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../database/models');
-require('dotenv').config(); 
+const db = require("../database/models");
+require("dotenv").config();
 
+router.get("/test", (req, res) => {
+  res.send("test");
+});
 
+router.get("/all", async (req, res) => {
+  const allPosts = await db.Post.findAll();
+  res.send(allPosts);
+});
 
-router.get('/test', (req, res)=>{
-    res.send('test')
-})
-
-router.get('/all', async(req, res)=>{
-     const allPosts = await db.Post.findAll()
-    res.send(allPosts)
-})
-
+router.get(`/color/:color`, async (req, res) => {
+  const color = req.params.color;
+  const colorSearch = await db.Post.findAll({
+    where: {
+      color: color,
+    },
+  });
+  res.send(colorSearch)
+});
 
 
 module.exports = router;
