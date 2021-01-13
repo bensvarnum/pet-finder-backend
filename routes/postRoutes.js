d9ef904b3c1d22c653f1edf58c0c2c84d53b77c9
Daniel Gallagher <danielbgallagher@gmail.com>
2021-01-12 19:00:05 -0500

    Post type and status type GET routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,18 +3,20 @@ const router = express.Router();
 const db = require("../database/models");
 require("dotenv").config();
 
+const post = db.Post;
+
 router.get("/test", (req, res) => {
   res.send("test");
 });
 
 router.get("/all", async (req, res) => {
-  const allPosts = await db.Post.findAll();
+  const allPosts = await post.findAll();
   res.send(allPosts);
 });
 
 router.get(`/color/:color`, async (req, res) => {
   const color = req.params.color;
-  const colorSearch = await db.Post.findAll({
+  const colorSearch = await post.findAll({
     where: {
       color: color,
     },
@@ -24,7 +26,7 @@ router.get(`/color/:color`, async (req, res) => {
 
 router.get(`/size/:size`, async (req, res) => {
   const size = req.params.size;
-  const sizeSearch = await db.Post.findAll({
+  const sizeSearch = await post.findAll({
     where: {
       size: size,
     },
@@ -34,12 +36,60 @@ router.get(`/size/:size`, async (req, res) => {
 
 router.get(`/name/:name`, async (req, res) => {
   const name = req.params.name;
-  const nameSearch = await db.Post.findAll({
+  const nameSearch = await post.findAll({
     where: {
       petName: name,
     },
   });
   res.send(nameSearch);
+});
+
+router.get('/limit/:number', async (req,res)=>{
+
+})
+
+router.get('/resolved', async(req, res)=>{
+  const resolved = await post.findAll({
+    where: {
+      StatusTypeId: 1
+    }
+  }
+  );
+
+  res.send(resolved);
+});
+
+router.get('/unresolved', async(req, res)=>{
+  const unresolved = await post.findAll({
+    where: {
+      StatusTypeId: 2
+    }
+  }
+  );
+
+  res.send(unresolved);
+});
+
+router.get('/lost', async(req, res)=>{
+  const lost = await post.findAll({
+    where: {
+      PostTypeId: 1
+    }
+  }
+  );
+
+  res.send(lost);
+});
+
+router.get('/found', async(req, res)=>{
+  const found = await post.findAll({
+    where: {
+      PostTypeId: 2
+    }
+  }
+  );
+
+  res.send(found);
 });
 
 module.exports = router;
