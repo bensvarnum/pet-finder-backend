bc65fd64a0553f6f2e493a384abf8f8ad941bd73
Daniel Gallagher <danielbgallagher@gmail.com>
2021-01-14 19:48:56 -0500

    added limits to routes

--- routes/GETpostRoutes.js
+++ routes/GETpostRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../database/models");
+const { search } = require("./OTHERpostRoutes");
 require("dotenv").config();
 
 const post = db.Post;
@@ -14,75 +15,52 @@ router.get("/all", async (req, res) => {
   res.send(allPosts);
 });
 
-router.get(`/color/:color`, async (req, res) => {
+router.get(`/color/:color/:limit`, async (req, res) => {
   const color = req.params.color;
   const colorSearch = await post.findAll({
     where: {
       color: color,
     },
+    limit: req.params.limit
   });
   res.send(colorSearch);
 });
 
-router.get(`/size/:size`, async (req, res) => {
+router.get(`/size/:size/:limit`, async (req, res) => {
   const size = req.params.size;
   const sizeSearch = await post.findAll({
     where: {
       size: size,
     },
+    limit: req.params.limit
   });
   res.send(sizeSearch);
 });
 
-router.get(`/name/:name`, async (req, res) => {
+router.get(`/name/:name/:limit`, async (req, res) => {
   const name = req.params.name;
   const nameSearch = await post.findAll({
     where: {
       petName: name,
     },
+    limit: req.params.limit
   });
   res.send(nameSearch);
 });
 
-router.get('/limit/posttype/:type/:limit', async (req,res)=>{
-  const type = req.params.type;
-  const limit = req.params.limit;
-
-  const limitPosts = await post.findAll({
-    where: {
-      PostTypeId: type,
-      StatusId: 2
-    },
-    limit: limit
-  })
-  res.send(limitPosts);
-})
-
-router.get('/limit/status/:type/:limit', async (req,res)=>{
-  const type = req.params.type;
-  const limit = req.params.limit;
-
-  const limitPosts = await post.findAll({
-    where: {
-      StatusId: type
-    },
-    limit: limit
-  })
-  res.send(limitPosts);
-})
-
-router.get('/resolved', async(req, res)=>{
+router.get('/resolved/:limit', async(req, res)=>{
   const resolved = await post.findAll({
     where: {
       StatusId: 1
-    }
+    },
+    limit: req.params.limit
   }
   );
 
   res.send(resolved);
 });
 
-router.get('/unresolved', async(req, res)=>{
+router.get('/unresolved/:limit', async(req, res)=>{
   const unresolved = await post.findAll({
     where: {
       StatusId: 2
@@ -93,22 +71,24 @@ router.get('/unresolved', async(req, res)=>{
   res.send(unresolved);
 });
 
-router.get('/lost', async(req, res)=>{
+router.get('/lost/:limit', async(req, res)=>{
   const lost = await post.findAll({
     where: {
       PostTypeId: 1
-    }
+    },
+    limit: req.params.limit
   }
   );
 
   res.send(lost);
 });
 
-router.get('/found', async(req, res)=>{
+router.get('/found/:limit', async(req, res)=>{
   const found = await post.findAll({
     where: {
       PostTypeId: 2
-    }
+    },
+    limit: req.params.limit
   }
   );
 
