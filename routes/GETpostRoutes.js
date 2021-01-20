2ba52d7a4191bfe40e48edb4aa47330073bc9eec
Daniel Gallagher <danielbgallagher@gmail.com>
2021-01-19 19:33:15 -0500

    changed file structure

--- routes/GETpostRoutes.js
+++ routes/GETpostRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../database/models");
+const db = require("../models");
 const { search } = require("./OTHERpostRoutes");
 require("dotenv").config();
 
@@ -21,7 +21,7 @@ router.get(`/color/:color/:limit`, async (req, res) => {
     where: {
       color: color,
     },
-    limit: req.params.limit
+    limit: req.params.limit,
   });
   res.send(colorSearch);
 });
@@ -32,7 +32,7 @@ router.get(`/size/:size/:limit`, async (req, res) => {
     where: {
       size: size,
     },
-    limit: req.params.limit
+    limit: req.params.limit,
   });
   res.send(sizeSearch);
 });
@@ -43,54 +43,50 @@ router.get(`/name/:name/:limit`, async (req, res) => {
     where: {
       petName: name,
     },
-    limit: req.params.limit
+    limit: req.params.limit,
   });
   res.send(nameSearch);
 });
 
-router.get('/resolved/:limit', async(req, res)=>{
+router.get("/resolved/:limit", async (req, res) => {
   const resolved = await post.findAll({
     where: {
-      StatusId: 1
+      StatusId: 1,
     },
-    limit: req.params.limit
-  }
-  );
+    limit: req.params.limit,
+  });
 
   res.send(resolved);
 });
 
-router.get('/unresolved/:limit', async(req, res)=>{
+router.get("/unresolved/:limit", async (req, res) => {
   const unresolved = await post.findAll({
     where: {
-      StatusId: 2
-    }
-  }
-  );
+      StatusId: 2,
+    },
+  });
 
   res.send(unresolved);
 });
 
-router.get('/lost/:limit', async(req, res)=>{
+router.get("/lost/:limit", async (req, res) => {
   const lost = await post.findAll({
     where: {
-      PostTypeId: 1
+      PostTypeId: 1,
     },
-    limit: req.params.limit
-  }
-  );
+    limit: req.params.limit,
+  });
 
   res.send(lost);
 });
 
-router.get('/found/:limit', async(req, res)=>{
+router.get("/found/:limit", async (req, res) => {
   const found = await post.findAll({
     where: {
-      PostTypeId: 2
+      PostTypeId: 2,
     },
-    limit: req.params.limit
-  }
-  );
+    limit: req.params.limit,
+  });
 
   res.send(found);
 });
--- routes/OTHERpostRoutes.js
+++ routes/OTHERpostRoutes.js
@@ -1,42 +1,50 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../database/models");
+const db = require("../models");
 require("dotenv").config();
 
 const post = db.Post;
 
-router.post('/add', async(req, res)=>{
-    console.log(req.body)
-    const { color, size, location, petName, email, phoneNumber, PostTypeId } = req.body;
-    const { mimetype, originalname, buffer } = req.file;
+router.post("/add", async (req, res) => {
+  console.log(req.body);
+  const {
+    color,
+    size,
+    location,
+    petName,
+    email,
+    phoneNumber,
+    PostTypeId,
+  } = req.body;
+  const { mimetype, originalname, buffer } = req.file;
 
-    const newPost = await post.create({
-        color,
-        size,
-        location,
-        petName,
-        email,
-        phoneNumber,
-        PostTypeId,
-        StatusId: 2,
-        imageType: mimetype,
-        imageName: originalname,
-        imageData: buffer
-    })
+  const newPost = await post.create({
+    color,
+    size,
+    location,
+    petName,
+    email,
+    phoneNumber,
+    PostTypeId,
+    StatusId: 2,
+    imageType: mimetype,
+    imageName: originalname,
+    imageData: buffer,
+  });
 
-    res.send('New Post created');
-})
+  res.send("New Post created");
+});
 
-router.patch('/update/:id', async(req, res)=>{
-    const { id } = req.params;
+router.patch("/update/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const updatePost = await post.update(req.body, {
-        where: {
-            id
-        }
-    });
-    
-    res.send("Updated");
-})
+  const updatePost = await post.update(req.body, {
+    where: {
+      id,
+    },
+  });
+
+  res.send("Updated");
+});
 
 module.exports = router;
