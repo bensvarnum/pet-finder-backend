fe6ef647eda8de1d9d401fed7418b2fd10fb017e
Daniel Gallagher <danielbgallagher@gmail.com>
2021-01-21 18:51:33 -0500

    typo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const OTHERpostRoutes = require("./routes/OTHERpostRoutes");
 
 app.use("/posts", OTHERpostRoutes);
 app.use("/posts", GETpostRoutes);
-app.use(cors);
+app.use(cors());
 
 app.listen(process.env.PORT, () => {
   console.log("Server Running on port: " + process.env.PORT);
