82c2be413af41e22f5acbaaea8875f559bdb0102
Daniel Gallagher <danielbgallagher@gmail.com>
2021-01-22 19:23:53 -0500

    removed multer

--- routes/OTHERpostRoutes.js
+++ routes/OTHERpostRoutes.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-var multer = require("multer");
-var upload = multer({ dest: "uploads/" });
 require("dotenv").config();
 
 const post = db.Post;
 
-router.post("/add", upload.single("pet"), async (req, res) => {
+router.post("/add", async (req, res) => {
   console.log(req.body);
-  console.log(req.file);
   const {
     color,
     size,
@@ -19,7 +16,6 @@ router.post("/add", upload.single("pet"), async (req, res) => {
     phoneNumber,
     PostTypeId,
   } = req.body;
-  const { mimetype, originalname, buffer } = req.file;
 
   const newPost = await post.create({
     color,
@@ -30,9 +26,6 @@ router.post("/add", upload.single("pet"), async (req, res) => {
     phoneNumber,
     PostTypeId,
     StatusId: 2,
-    imageType: mimetype,
-    imageName: originalname,
-    imageData: buffer,
   });
 
   res.send("New Post created");
