a3a7ae6bda789f695098b4ba7f5bd1af139c9c53
bensvarnum <bens.varnum@gmail.com>
2021-01-24 18:20:53 -0500

    add url and description

---
+++ migrations/20210124231612-modify-post-table-add-fields.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn("Posts", "url", {
+      type: Sequelize.STRING,
+    });
+    await queryInterface.addColumn("Posts", "description", {
+      type: Sequelize.STRING,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn("Posts", "url"),
+      await queryInterface.removeColumn("Posts", "description");
+  },
+};
