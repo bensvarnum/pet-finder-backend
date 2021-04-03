54f03a19af3a09e25c6b6fb39c98f890c1b72026
bensvarnum <bens.varnum@gmail.com>
2021-04-02 23:18:51 -0400

    fixes cors error

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
-const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors());
-app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extened: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 const GETpostRoutes = require("./routes/GETpostRoutes");
 const OTHERpostRoutes = require("./routes/OTHERpostRoutes");
